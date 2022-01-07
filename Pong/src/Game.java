316210bc279a076eec687aa0763877b7f0d82263
Rohaan <768840@pdsb.net>
2022-01-07 13:34:24 -0500

    Created class Game Created class Window * Will rename the files to include View when we're finished. - Rohaan

---
+++ Pong/src/Game.java
@@ -0,0 +1,101 @@
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+public class Game extends Canvas implements Runnable{
+
+    // Variable Declaration
+    public static int WIDTH = 800, HEIGHT = 608;
+    public String title = "Pong";
+
+    private Thread thread;
+    private boolean isRunning = false;
+
+    // Constructor
+    public Game(){
+        new Window(WIDTH, HEIGHT, title, this);
+        start();
+    }
+
+    private synchronized void start(){
+        if(isRunning) return;
+
+        thread = new Thread(this, "Game");
+        thread.start();
+        isRunning = true;
+    }
+
+    private synchronized void stop(){
+        if(!isRunning) return;
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        isRunning = false;
+    }
+
+    // Game Loop
+    public void run() {
+        this.requestFocus();
+
+        // Variable Declaration
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+
+        // Loop to render and update game
+        while(isRunning){
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+
+            while(delta >= 1){
+                tick();
+                delta--;
+            }
+
+            render();
+            frames++;
+
+            if(System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                frames = 0;
+            }
+        }
+
+        stop();
+    }
+
+    // Updates the game
+    private void tick(){
+
+    }
+
+    // Renders the game
+    private void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+
+        if(bs == null){
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+
+        // Drawing the game
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        bs.show();
+        g.dispose();
+    }
+
+    // Main
+    public static void main(String[] args){
+        new Game();
+    }
+}
