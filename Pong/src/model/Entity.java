a649fae1c1d90f674532f950769143321eb4eeeb
plurb <36715300+plurb@users.noreply.github.com>
2022-01-07 20:52:25 -0500

    AABB implemented (collision detection)
    Signed-off-by: plurb <36715300+plurb@users.noreply.github.com>

---
+++ Pong/src/model/Entity.java
@@ -0,0 +1,8 @@
+package model;
+
+import util.math.AABB;
+
+public class Entity
+{
+    public AABB rect;
+}
---
+++ Pong/src/model/Paddle.java
@@ -0,0 +1,8 @@
+package model;
+
+import util.math.Vector2;
+
+public class Paddle extends Entity
+{
+    Vector2 velocity;
+}
