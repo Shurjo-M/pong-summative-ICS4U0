de1ff93b0999e9a36718efba44ba0c5805c5b41c
plurb <36715300+plurb@users.noreply.github.com>
2022-01-07 20:42:20 -0500

    AABB implemented (collision detection)
    Signed-off-by: plurb <36715300+plurb@users.noreply.github.com>

--- Pong/src/util/math/AABB.java
+++ Pong/src/util/math/AABB.java
@@ -2,14 +2,22 @@ package util.math;
 
 public class AABB
 {
-    Vector2 offset;
-    float width, height;
+    float x, y;
+    float w, h;
 
     /*
      * TODO: add methods to check for"
      *     - Collision with other AABB
      *     - Collision with point (Vec2)
      */
+
+    public boolean intersects(AABB rect)
+    {
+        return this.x < rect.x + rect.w &&
+               this.x + this.w > rect.x &&
+               this.y < rect.y + rect.h &&
+               this.h + this.y > rect.y;
+    }
 
 }
 
