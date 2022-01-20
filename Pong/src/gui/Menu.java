714458b8c0a20f2165cd108da464944f54b55c02
Rohaan <768840@pdsb.net>
2022-01-20 12:29:41 -0500

    - Finally finished menu, kinda

--- Pong/src/gui/Menu.java
+++ Pong/src/gui/Menu.java
@@ -2,6 +2,7 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 public class Menu extends JPanel
 {
@@ -9,6 +10,9 @@ public class Menu extends JPanel
     JButton start = new JButton("START");
     JButton main_menu = new JButton("MAIN MENU");
     JButton quit = new JButton("QUIT");
+    Graphics g = this.getGraphics();
+    JPanel bar1 = new BarPanel1();
+    JPanel bar2 = new BarPanel2();
     JPanel buttons = new JPanel();
     JPanel titleP = new JPanel();
 
@@ -28,7 +32,7 @@ public class Menu extends JPanel
         titleP.add(this.title);
 
         // Buttons Panel
-        buttons.setLayout(new GridLayout(3,1));
+        buttons.setLayout(new GridLayout(2,1));
 
         start.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         buttons.add(this.start);
@@ -36,10 +40,22 @@ public class Menu extends JPanel
         quit.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         buttons.add(this.quit);
 
+        // Bar Panels
+        bar1.setLayout(new FlowLayout());
+        bar1.setBackground(Color.black);
+        bar1.setBorder(BorderFactory.createEmptyBorder(50,50,50,50));
+
+        bar2.setLayout(new GridBagLayout());
+        bar2.setBackground(Color.black);
+        bar2.setBorder(BorderFactory.createEmptyBorder(50,150,50,50));
+
         // Complete Layout
         this.setLayout(new BorderLayout());
+        this.setBackground(Color.black);
         this.add(this.titleP, BorderLayout.NORTH);
         this.add(this.buttons, BorderLayout.CENTER);
+        this.add(this.bar1, BorderLayout.EAST);
+        this.add(this.bar2, BorderLayout.WEST);
     }
 
 
@@ -55,5 +71,45 @@ public class Menu extends JPanel
     private void update()
     {
 
+    }
+
+    class BarPanel1 extends JPanel{
+        BarPanel1(){
+            setPreferredSize(new Dimension(300, 600));
+        }
+
+        @Override
+        public void paintComponent(Graphics g){
+            super.paintComponent(g);
+
+            g.setColor(Color.white);
+            g.fillRect(30, 50, 70, 200);
+        }
+    }
+
+    class BarPanel2 extends JPanel{
+        BarPanel2(){
+            setPreferredSize(new Dimension(300, 600));
+        }
+
+        @Override
+        public void paintComponent(Graphics g){
+            super.paintComponent(g);
+
+            g.setColor(Color.white);
+            g.fillRect(130, 50, 70, 200);
+        }
+    }
+
+
+    public static void main(String[] args){
+        Menu menu = new Menu();
+
+        JFrame main = new JFrame();
+        main.setContentPane(menu);
+        main.setTitle("Hangman");
+        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main.setSize(800, 600);
+        main.setVisible(true);
     }
 }
