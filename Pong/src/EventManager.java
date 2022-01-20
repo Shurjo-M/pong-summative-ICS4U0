d3a3bfd5540c57cf4d853a96e0627f23efb8a6b0
plurb <36715300+plurb@users.noreply.github.com>
2022-01-19 23:41:50 -0500

    quick fixes and the general agony that is handling input in a remotely useful way
    Signed-off-by: plurb <36715300+plurb@users.noreply.github.com>

---
+++ Pong/src/EventManager.java
@@ -0,0 +1,28 @@
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
+
+import static util.math.Units.booltf;
+
+public class EventManager
+{
+    HashMap<Integer, Float> actions;
+
+    public EventManager()
+    {
+        actions = new HashMap<>();
+    }
+
+    public void update(KeyEvent event)
+    {
+        var code = event.getKeyCode();
+        if (actions.containsKey(code))
+        {
+            actions.replace(event.getKeyCode(), booltf(event.getID() == KeyEvent.KEY_PRESSED));
+        }
+    }
+
+    public void track(int event)
+    {
+        actions.putIfAbsent(event, 0f);
+    }
+}
